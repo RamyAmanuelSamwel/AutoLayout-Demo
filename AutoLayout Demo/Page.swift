f26de8145751db442dee33907d3c7c3f2410fe92
RamyAmanuelSamwel <remenwel66@hotmail.com>
2019-06-26 12:30:15 +0200

    initial commit

---
+++ AutoLayout Demo/Page.swift
@@ -0,0 +1,17 @@
+//
+//  Page.swift
+//  AutoLayout Demo
+//
+//  Created by Ramy Amanuel Samwel George on 6/26/19.
+//  Copyright © 2019 RamyAmanuelSamwel. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    
+    let imageName: String
+    let headerText: String
+    let bodyText: String
+    
+}
